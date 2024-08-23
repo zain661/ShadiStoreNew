9b6eb4bd6b6acc203f2ce0fae9262d297ccae0de
Zayn Assaf <67260716+zain661@users.noreply.github.com>
2024-08-23 16:07:54 +0300

    Update Register.jsx

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -16,7 +16,7 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('https://shadi-store-new.vercel.app/register', {
+            const response = await fetch('process.env.SERVER_URL/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'

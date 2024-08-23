639cc8f2b64d50a95c84930088fd1236ad5e5ae9
Zain Assaf <zaynassaf2001@gmail.com>
2024-08-23 19:22:28 +0300

    edit image path

--- backend/index.mjs
+++ backend/index.mjs
@@ -447,7 +447,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    image_url: `${process.env.CLIENT_URL}/images/${req.file.filename}`,
   });
 });
 app.post("/addproduct", async (req, res) => {

b6c2ad371d3a0d3f70354bc87fad11eda6fa1ea8
Zayn Assaf <67260716+zain661@users.noreply.github.com>
2024-08-23 16:10:13 +0300

    Update VerifyEmail.jsx

--- frontend/src/Pages/VerifyEmail.jsx
+++ frontend/src/Pages/VerifyEmail.jsx
@@ -12,7 +12,7 @@ const VerifyEmail = () => {
         const token = query.get('token');
 
         if (token) {
-            fetch(`https://shadi-store-new.vercel.app/verify-email?token=${token}`)
+            fetch(`${process.env.SERVER_URL}/verify-email?token=${token}`)
                 .then(response => response.json())
                 .then(data => {
                     if (data.message) {
